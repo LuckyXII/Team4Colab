2b5fbf580a3a5c07dd1430c35b549c5efa578d05
LuckyXII <omnipotent117@gmail.com>
2017-05-02 16:25:55 +0200

    integrate spotify in index and translate index to React

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 /*jshint esnext: true, moz: true*/
 /*jslint browser:true */
-/*global firebase */
+/*global firebase, fetch */
 
 //=======================================================
 //GLOBALS
 const logInButton = document.getElementById('log-in');
 const userContainer = document.getElementById('container');
+const searchInput = document.getElementById("main-search");
+const radioBtns = document.getElementsByClassName("radio-btn");
+const resultTable = document.getElementById("resultTable");
+const searchBtn = document.getElementById("searchBtn");
 
 //=======================================================
 //MAIN
 
+
 //=======================================================
 //CALLBACKS
+searchBtn.addEventListener("click", findResults);
 
 //=======================================================
 //FIREBASE
@@ -44,10 +50,132 @@ logInButton.addEventListener('click', () => {
         console.log(error);
     });
 });
+//END login/logout
 
 //=======================================================
 //FUNCTIONS
 
+//------------------------------------
+//SPOTIFY
+
+function findResults(){
+    
+    
+    let searchType;
+    let inputValue = "";
+    let title = "";
+    let artist = "";
+    let album = "";
+    let cover = "";
+    
+    //if empty search
+    if(searchInput !== ""){
+        inputValue = searchInput.value;    
+    }
+    
+    //check for selected radioBtn
+    for(let i = 0; i < radioBtns.length; i++){
+        if(radioBtns[i].checked === true){
+            searchType = radioBtns[i].value;
+        }
+
+    }
+    
+    let url = `https://api.spotify.com/v1/search?q=${inputValue}&type=${searchType}&limit=5`;
+    
+    fetch(url)
+    .then((response)=> {
+    	return response.json();
+    })
+    .then((result)=> {
+    	console.log(result);
+        
+        //Track
+        if(searchType === "track"){
+            
+            let tracks = result.tracks.items;
+            
+            for(let i = 0; i < tracks.length; i++){
+                
+                let item = document.createElement("tr");
+                title = tracks[i].name;
+                artist = tracks[i].artists[0].name;
+                album = tracks[i].album.name;
+                
+                if(tracks[i].album.images.length !== 0){
+                    cover = tracks[i].album.images[0].url;    
+                }
+                
+                item.innerHTML = `
+                                    <td>${title}</td>
+                                    <td>${artist}</td>
+                                    <td>${album}</td>
+                                    <td>YouTube</td>
+                                    <td>Spotify</td>
+                                    <td><i class="material-icons">favorite_border</i></td>
+                                `;
+                resultTable.appendChild(item);
+                
+            }
+            
+        }
+        //Artist
+        else if(searchType === "artist"){
+            let artists = result.artists.items;
+            
+            for(let i = 0; i < artists.length; i++){
+                let item = document.createElement("tr");
+                artist = artists[i].name;
+                
+                if(artists[i].images.length !== 0){
+                    cover = artists[i].images[0].url;    
+                }
+                
+                item.innerHTML = `
+                                    <td><img class="coverPic" src="${cover}" alt="cover"/></td>
+                                    <td>${artist}</td>
+                                    <td>YouTube</td>
+                                    <td>Spotify</td>
+                                `;
+               resultTable.appendChild(item);
+            }
+        
+            
+        }
+        //Album
+        else if(searchType === "album"){
+            let albums = result.albums.items;
+            
+            for(let i = 0; i < albums.length; i++){
+                let item = document.createElement("tr");
+                artist = albums[i].artists[0].name;
+                album = albums[i].name;
+                
+                if(albums[i].images.length !== 0){
+                    cover = albums[i].images[0].url;    
+                }
+                
+                item.innerHTML = `
+
+                                    <td><img class="coverPic" src="${cover}" alt="cover"/></td>
+                                    <td>${artist}</td>
+                                    <td>${album}</td>
+                                    <td>YouTube</td>
+                                    <td>Spotify</td>
+                                `;
+                
+                resultTable.appendChild(item);
+            }
+            
+        }
+        
+        
+        inputValue = "";
+        
+        
+    });
+    
+}
 
 
 
